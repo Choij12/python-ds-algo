460b4ebafdc0d2c6eb9c891def0053ec6840366b
Jacob Choi <jacob.j.choi@gmail.com>
2022-06-01 14:17:53 -0700

    passing own tests

--- python/code_challenges/hashtable_left_join.py
+++ python/code_challenges/hashtable_left_join.py
@@ -1,5 +1,15 @@
-from data_structures.hashtable import Hashtable
+# from data_structures.hashtable import Hashtable
 
+def left_join(left_hash, right_hash):
+    hashtable_combine = []
 
-def left_join():
-    pass
+    for key in left_hash:
+        hashtable_combine.append(key)
+        hashtable_combine.append(left_hash[key])
+
+        if key in right_hash:
+            hashtable_combine.append(right_hash[key])
+        else:
+            hashtable_combine.append(None)
+
+    return hashtable_combine
